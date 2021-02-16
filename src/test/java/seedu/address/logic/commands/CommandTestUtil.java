5b7d4b3dc9931863618a81180248841122ddd02a
Joel Ho <joelhoengkiat@gmail.com>
2021-02-16 21:13:45 +0800

    Include tests for RemarkCommand

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -36,6 +36,7 @@ public class CommandTestUtil {
     public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_REMARK = "aa";
+    public static final String VALID_REMARK_BOB = "bb";
     public static final String VALID_TAG_HUSBAND = "husband";
     public static final String VALID_TAG_FRIEND = "friend";
 
